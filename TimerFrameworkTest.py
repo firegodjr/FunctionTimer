9413b933f20d0fe6ef3aa4e3aa5c4c6d479d7f50
Ethan S <nerfsniper@att.net>
2019-11-21 12:14:16 -0600

    Fixed broken files, mostly (thanks Floobits)

--- TimerFrameworkTest.py
+++ TimerFrameworkTest.py
@@ -1,75 +1,12 @@
-# Pre-condition:
-# Post-condition:
-
-import sys
+import neattime
 
 
-def print_optimal_parens(s, i, j):
-    if i == j:
-        print("A" + i)
-    else:
-        print("(" + print_optimal_parens(s, i, s[i][j]) + print_optimal_parens(s, s[i][j] + 1, j) + ")")
-    return
+filepath = input("Enter the name of the test file you want to use: ")
+tester = neattime.TimeTester(filepath)
+results = tester.test_all(300)
 
-
-########################################################################################################################
-
-def recursive_matrix_chain(p, i, j, m):
-    if i == j:
-        return 0
-    m[i][j] = sys.maxsize
-    for k in range(i, j-1):
-        q = recursive_matrix_chain(p, i, k) + recursive_matrix_chain(p, k + 1, j) + (p[i - 1])(p[k])(p[j])
-        if q < m[i][j]:
-            m[i][j] = q
-    return q
-
-
-########################################################################################################################
-
-
-def bottom_up_mcm(p, m, s, n):
-    for i in range(1, n):
-        m[i][i] = 0
-    for l in range(2, n):
-        for i in range(1, n - l + 1):
-            j = i + l - 1
-            m[i][j] = sys.maxsize
-            for k in range (i, j - 1):
-                q = m[i][k] + m[k+1][j] + (p[i-1])(p[k])(p[j])
-                if q < m[i][j]:
-                    m[i][j] = q
-                    s[i][j] = k
-    return m
-
-
-########################################################################################################################
-
-
-def memoized_matrix_chain(p, m, n):
-    for i in range(1, n):
-        for j in range(i, n):
-            m[i][j] = sys.maxsize
-    return lookup_chain(m, p, 1, n)
-
-
-def lookup_chain(m, p, i, j):
-    if m[i][j] < sys.maxsize:
-        return m[i][j]
-    if i == j:
-        m[i][j] = 0
-    else:
-        for k in range(i, j-1):
-            q = lookup_chain(m, p, i, k) + lookup_chain(m, p, k+1, j) + (p[i-1])(p[k])(p[j])
-            if q < m[i][j]:
-                m[i][j] = q
-    return m[i][j]
-
-
-########################################################################################################################
-
-
-def m_matrix_    for i in range(1,len(m)):0
-scanner(m):
-    for i in range(1,len(m)):0
-
+csv = ""
+for case in results.keys():
+    csv += "{},\n".format(case)
+    for arg in results[case].keys():
+        csv += ",{},{},\n".format(arg, ",".join(results[case][arg]))
